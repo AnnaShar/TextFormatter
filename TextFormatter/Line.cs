503e1f85f918adfe79b18ca62f58644a70fc38e5
AnnaShar <sharuntsova.anna@yandex.ru>
2018-11-10 13:58:56 +0300

    Add classes Line and LineFormatter. Renew function Justify in TextFormatter.

---
+++ TextFormatter/Line.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace TextFormatter
+{
+    class Line
+    {
+        Queue<string> _wordsQueue;
+        int _numberOfSpaces;
+
+        public int Length { get; private set; }
+        public int AllWordsLength { get; private set; }
+        public int NumberOfWords
+        {
+            get
+            {
+                return _wordsQueue.Count;
+            }
+        }
+
+        public Line()
+        {
+            _wordsQueue = new Queue<string>();
+            _numberOfSpaces = 0;
+            AllWordsLength = 0;
+            Length = 0;
+        }
+
+        public void AddWord(string word)
+        {
+            _wordsQueue.Enqueue(word);
+            _numberOfSpaces++;
+            AllWordsLength += word.Length;
+            Length = AllWordsLength + _numberOfSpaces;
+        }
+        public string GetWord()
+        {
+            if (_wordsQueue.Count == 0)
+                 throw new InvalidOperationException("Line is empty.");
+
+            string word = _wordsQueue.Dequeue();
+            AllWordsLength -= word.Length;
+            _numberOfSpaces--;
+            Length = AllWordsLength + _numberOfSpaces;
+
+            return word;
+        }
+        public void Clear()
+        {
+            _wordsQueue.Clear();
+            _numberOfSpaces = 0;
+            AllWordsLength = 0;
+            Length = 0;
+        }
+        public string[] ToArray ()
+        {
+            return _wordsQueue.ToArray();
+        }
+    }
+}
---
+++ TextFormatter/LineFormatter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFormatter
+{
+    class LineFormatter
+    {
+        int _width;
+        public LineFormatter(int width)
+        {
+            _width = width;
+        }
+
+        public string FormatLine(Line line)
+        {
+            string resultLine = "";
+            int numberOfWords = line.NumberOfWords;
+            int numberOfGaps = numberOfWords - 1;
+
+            if (numberOfGaps == 0)
+                return line.GetWord() + "\r\n";
+
+            int averageGapLength = (_width - line.AllWordsLength) / numberOfGaps;
+            int allGapsLength = _width - line.AllWordsLength;
+            int numberOfBiggerGaps = allGapsLength - averageGapLength * numberOfGaps;
+
+            for (int i = 0; i < numberOfBiggerGaps; i++)
+                resultLine += GetWordWithGap(line.GetWord(), averageGapLength + 1);
+
+            for (int i = numberOfBiggerGaps; i < numberOfWords - 1; i++)
+                resultLine += GetWordWithGap(line.GetWord(), averageGapLength);
+
+            resultLine += line.GetWord() + "\r\n";
+            return resultLine;
+        }
+
+        private string GetWordWithGap(string word, int gapLength)
+        {
+            return word + new string(' ', gapLength);
+        }
+
+        public string FormatLastLine(Line line)
+        {
+            return string.Join(" ", line.ToArray());
+        }
+    }
+}
